e9ac7f35baa36d2a9defe53b91b784d0af304cd8
Lara Bozzano <104174013+lara-bozzano@users.noreply.github.com>
2022-04-23 17:19:09 -0300

    Initial commit

---
+++ src/ejercicio1.ts
@@ -0,0 +1,10 @@
+console.log("Receta torta");
+console.log("Enharinar y enmantecar el molde");
+console.log("En un bol añadir 2 huevos y 1 taza de azucar");
+console.log("Batir");
+console.log("Añadir 1 taza de leche y una cucharada de esencia de vainilla");
+console.log("Batir");
+console.log("Agregar 2 tazas de harina");
+console.log("Batir y unificar todo");
+console.log("Añadir la mezcla al molde");
+console.log("Cocinar en el horno medio durante 60 minutos");
